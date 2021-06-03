25df76517d36856dd92662fbae8723728a579982
Wendy <wendytsao84@gmail.com>
2021-06-03 19:59:12 +0800

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -6,4 +6,5 @@ import store from './store'
 createApp(App).use(store).use(router).mount('#app')
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './assets/css/style.css';
